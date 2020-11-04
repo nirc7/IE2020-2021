460da89896b3e2747485fc67eaf55d9376bb07ae
nirc7 <i2cs2013@gmail.com>
2020-11-04 15:11:16 +0200

    com2

--- 01 04_11_2020 Elements FC/src/FunctionalComponents/FCStudent.js
+++ 01 04_11_2020 Elements FC/src/FunctionalComponents/FCStudent.js
@@ -8,7 +8,7 @@ export default function FCStudent(props) {
 
  return (
   <div style={{border:'solid green 1px', margin:5}}>
-    <h2>Student</h2>
+    <h2>Student of Rup</h2>
     <h3>props.id= {props.id === undefined ? 'und': props.id}</h3>
     <h3>props.name= {props.name} { props.name === "avi" && 0 } </h3>
     <h3>props.inRup= {props.inRup ? 'yes':'no'}</h3>
